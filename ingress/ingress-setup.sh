cb661d5c06071aabbabe8ad7dc29f0fe478afc13
Mir Adnan ALI <maali@evenica.com>
2020-03-11 12:43:12 -0400

    variable namespace

--- ingress/ingress-setup.sh
+++ ingress/ingress-setup.sh
@@ -1,5 +1,9 @@
+
+
+export NS=ingress-1
+
 # Create separate namespace
-kubectl create namespace ingress-basic
+kubectl create namespace "$NS"
 
 
 # Add the official stable repository
@@ -7,7 +11,7 @@ helm repo add stable https://kubernetes-charts.storage.googleapis.com/
 
 # Use Helm to deploy an NGINX ingress controller
 helm install nginx-ingress stable/nginx-ingress \
-    --namespace ingress-basic \
+    --namespace "$NS" \
     --set controller.replicaCount=2 \
     --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
     --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux
@@ -15,7 +19,7 @@ helm install nginx-ingress stable/nginx-ingress \
 
 
 ##
-# $ kubectl get service -l app=nginx-ingress --namespace ingress-basic
+# $ kubectl get service -l app=nginx-ingress --namespace "$NS"
 # 
 # NAME              TYPE           CLUSTER-IP     EXTERNAL-IP   PORT(S)                      AGE
 # nginx-ingress-controller   LoadBalancer   10.0.61.144    EXTERNAL_IP   80:30386/TCP,443:32276/TCP   6m2s
@@ -26,28 +30,28 @@ helm install nginx-ingress stable/nginx-ingress \
 ## DEMO APPLICATION
 helm repo add azure-samples https://azure-samples.github.io/helm-charts/
 
-helm install aks-helloworld azure-samples/aks-helloworld --namespace ingress-basic
+helm install aks-helloworld azure-samples/aks-helloworld --namespace "$NS"
 
 helm install aks-helloworld-two azure-samples/aks-helloworld \
-    --namespace ingress-basic \
+    --namespace "$NS" \
     --set title="AKS Ingress Demo" \
     --set serviceName="aks-helloworld-two"
 
 kubectl apply -f hello-world-ingress.yaml
 
-export EXTERNAL_IP=$(kubectl get service -l app=nginx-ingress --namespace ingress-basic |awk '{print $4}' | head -2 | tail -1)
+export EXTERNAL_IP=$(kubectl get service -l app=nginx-ingress --namespace "$NS" |awk '{print $4}' | head -2 | tail -1)
 
 echo Visit $EXTERNAL_IP in your browser to see the demo app...
 
 read input
 
 ## Delete the sample namespace and all resources
-helm delete aks-helloworld aks-helloworld-two nginx-ingress --namespace ingress-basic
+helm delete aks-helloworld aks-helloworld-two nginx-ingress --namespace "$NS"
 
 
 helm repo remove azure-samples
 
 kubectl delete -f hello-world-ingress.yaml
 
-kubectl delete namespace ingress-basic
+kubectl delete namespace "$NS"
 
